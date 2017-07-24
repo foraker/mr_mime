a55e5c439a00d8c613444b84c02be43d4a03aa65
Kyle Edson <kae@foraker.com>
2017-07-24 14:12:30 -0700

    Devise Adapter: Ensure correct user class when ending impersonation

--- app/models/mr_mime/adapters/devise_adapter.rb
+++ app/models/mr_mime/adapters/devise_adapter.rb
@@ -1,9 +1,11 @@
 module MrMime
   module Adapters
     class DeviseAdapter < Base
+      delegate :user_class, to: MrMime::Config
+
       def set_current_user(user)
         context.sign_out
-        context.sign_in(user)
+        context.sign_in user.becomes(user_class)
       end
     end
   end
